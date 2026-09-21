f6697eab428ea7267127417c86d11226cd82180a
Hacksmith55555 <pureprogaming25@gmail.com>
2026-09-21 05:29:58 -0500

    Changed to using flask with Excel

--- test.py
+++ test.py
@@ -2,6 +2,14 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route('/')
+def index():
+    return 'Do you love me?? <a href="/yes"> <button type="button">Yes</ button> </a> <a href="/no"> <button type="button">No</ button> </a>'
+
+@app.route('/no')
+def no():
+    return 'fuck you try again <a href="/"> <button type="button">Try Again</ button> </a>'
+
+@app.route('/yes')
+def yes():
+    return 'Goooood Girllll'
